e8fe15ebb61c39c0a0e5a3d368b57a1c7e58779b
gporras <g.porras@nfb.ca>
2016-05-19 09:10:15 -0400

    Added Pod KalturaPlayerSDK

---
+++ SwiftVideoPlayer/Pods/Target Support Files/Pods-SwiftVideoPlayer/Pods-SwiftVideoPlayer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftVideoPlayer : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftVideoPlayer
+@end
---
+++ SwiftVideoPlayer/SwiftVideoPlayer/KalturaplayerSDK-Bridging-Header.m
@@ -0,0 +1,9 @@
+//
+//  KalturaplayerSDK-Bridging-Header.m
+//  SwiftVideoPlayer
+//
+//  Created by Gerado Cepeda Porras on 2016-05-18.
+//  Copyright © 2016 PrashantKumar Mangukiya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
